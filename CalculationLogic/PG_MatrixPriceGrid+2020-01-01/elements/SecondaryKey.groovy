5bc204a5d31719c362f0c99e9db0b38a3dc5337b
DarthCoderAdy <itcodingstudent@gmail.com>
2024-07-24 09:27:37 +0200

    pfx-Advanced_CE - MatrixPriceList - Lab - adding Matrix Price Grid definition

--- CalculationLogic/PG_MatrixPriceGrid+2020-01-01/elements/SecondaryKey.groovy
+++ CalculationLogic/PG_MatrixPriceGrid+2020-01-01/elements/SecondaryKey.groovy
@@ -3,7 +3,7 @@ if (!api.isDebugMode()) {
 } else {
     def volumeBreaks = api.findLookupTableValues("VolumeBreaks", "VolumeBreak")?.key3?.unique()
     if (api.isInputGenerationExecution()) {
-        api.option("VolumeBreak", volumeBreaks)
+        return api.option("VolumeBreak", volumeBreaks)
     } else {
         return input.VolumeBreak
     }
