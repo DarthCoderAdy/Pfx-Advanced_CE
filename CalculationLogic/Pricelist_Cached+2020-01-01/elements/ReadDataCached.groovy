5207e0b94c5eb133331c9a019962b29ce825e78b
DarthCoderAdy <itcodingstudent@gmail.com>
2024-07-25 16:11:28 +0200

    pfx-Advanced_CE - Performance, Caching in api.global - Lab

---
+++ CalculationLogic/Pricelist_Cached+2020-01-01/elements/ReadDataCached.groovy
@@ -0,0 +1,20 @@
+def getAllFreightSurcharges() {
+return api.namedEntities(
+        api.findLookupTableValues("FreightSurcharge")
+)
+}
+
+def getCachedAllFreightSurcharges() {
+    final key = "FreightSurchargeDataCacheKey"
+
+    if (api.global.containsKey(key)) {
+        return api.global[key]
+    }
+
+    def data = getAllFreightSurcharges()
+
+    api.global[key] = data
+
+    return data
+}
+return getCachedAllFreightSurcharges()
